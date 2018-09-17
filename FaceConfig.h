ac6c284a95ff20cd065803d757995d924c6b05ee
David Huang <davidh6472@gmail.com>
2018-09-17 13:20:27 -0400

    Initial commit

---
+++ FaceConfig.h
@@ -0,0 +1,2 @@
+#define Face_VERSION_MAJOR 1
+#define Face_VERSION_MINOR 1
---
+++ face.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include "FaceConfig.h"
+
+using namespace std;
+
+int main() {
+    cout << "This works";
+    return 0;
+}
